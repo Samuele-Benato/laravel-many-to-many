b8b0f87f558eb567aa911a736d5335a7ce131337
Samuele-Benato <samuele.benato@gmail.com>
2023-10-27 15:17:58 +0200

    feat : add migrations

--- database/migrations/2023_10_27_095249_create_technologies_table.php
+++ database/migrations/2023_10_27_095249_create_technologies_table.php
@@ -12,16 +12,10 @@ return new class extends Migration {
      */
     public function up()
     {
-        Schema::create('project_technology', function (Blueprint $table) {
+        Schema::create('technologies', function (Blueprint $table) {
             $table->id();
-            
-            $table->foreignId('project_id')
-                ->constrained()
-                ->cascadeOnDelete();
-
-            $table->foreignId('technology_id')
-                ->constrained()
-                ->cascadeOnDelete();
+            $table->string('label', 20);
+            $table->timestamps();
         });
     }
 
@@ -32,6 +26,6 @@ return new class extends Migration {
      */
     public function down()
     {
-        Schema::dropIfExists('project_technology');
+        Schema::dropIfExists('technologies');
     }
 };
